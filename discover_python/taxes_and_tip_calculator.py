347a7df32dec999ce8d7706e54656876ea17481a
Kris Bredemeier <34@holbertonschool.com>
2016-02-06 10:22:36 -0800

    tip caculator created

---
+++ discover_python/taxes_and_tip_calculator.py
@@ -0,0 +1,38 @@
+import os
+#clear the screen
+os.system('clear')
+
+"""This is a tip calculator for meals
+written by Kris Bredemeier"""
+
+print("Welcome to the taxs and tip calculator!")
+
+#accounts for price of meal
+meal = float(raw_input("What is the price before tax? "))
+
+#accounts for tax
+tax = float(raw_input("What are the taxes? (in %) "))
+
+#converts tax to decimal
+tax = tax / 100
+
+#accoutns for meal, tax included
+meal = meal + meal * tax
+
+#accounts for tip
+tip = float(raw_input("What do you want to tip? (in %) "))
+
+#converts tip to decimal
+tip = tip / 100
+
+#accounts for meal and tip
+total = meal + meal * tip
+
+#prints total
+print "The prince you need to pay is: " +str(total)
+
+raw_input('Press enter to quit...')
+
+#clear the screen
+os.system('clear')
+

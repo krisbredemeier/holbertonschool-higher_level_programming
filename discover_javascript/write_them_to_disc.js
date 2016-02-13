33bec21281c8cc0ac3ef161bd646feef556d9326
Kris Bredemeier <34@holbertonschool.com>
2016-02-13 11:57:14 -0800

    saves file

---
+++ discover_javascript/write_them_to_disc.js
@@ -0,0 +1,30 @@
+var https = require ('https');
+
+var options = {
+  hostname: 'api.github.com',
+  path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
+  headers: {
+    'User-Agent': 'Holberton_School',
+    'Authorization': 'token 76cee11a2bc92e62615194f891ae4c0cd60cc9ab'
+  }
+};
+const chunks = [];
+var req = https.get(options, function(res) {
+  console.log(res.statusCode);
+  res.on('data', (chunk) => {
+     chunks.push(chunk);
+   });
+  res.on('end', () => {
+    var jsonString = chunks.join('');
+  });
+});
+  var fs = require('fs');
+  fs.writeFile('/tmp/32', 'jsonString', function(err) {
+    if(err) {
+      return console.log(err);
+    }
+    console.log("The file was saved!");
+});
+
+
+

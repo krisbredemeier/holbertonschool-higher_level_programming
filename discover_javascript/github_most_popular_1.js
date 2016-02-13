950afe7d013dbeac4653dc8a4e5839dae059bdb3
Kris Bredemeier <34@holbertonschool.com>
2016-02-13 10:27:17 -0800

    github most popular

---
+++ discover_javascript/github_most_popular_1.js
@@ -0,0 +1,23 @@
+var https = require ('https');
+
+var options = {
+  hostname: 'api.github.com',
+  path: '/search/repositories?q=language:javascript&sort=stars&order=desc',
+  headers: {
+    'User-Agent': 'Holberton_School',
+    'Authorization': 'token 76cee11a2bc92e62615194f891ae4c0cd60cc9ab'
+  }
+};
+var req = https.request(options, function(res) {
+  console.log(res.statusCode);
+  res.on('data', function(d) {
+    process.stdout.write(d);
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.error(e);
+});
+
+

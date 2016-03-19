f46efc1656e6dbb9b9d1584392b13615065dba04
Kris Bredemeier <34@holbertonschool.com>
2016-03-19 13:55:13 -0700

    writes to /tmp file

---
+++ discover_ruby/write_them_to_disc.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'httpclient'
+
+File.open('/tmp/34', 'w') do |file|
+clnt = HTTPClient.new
+file.puts clnt.get_content('https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc')
+puts "The file was saved!"
+end

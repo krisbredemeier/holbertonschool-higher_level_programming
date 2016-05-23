a8adb3be33944b9dfeaf1f483a16585383ddefea
Kris Bredemeier <34@holbertonschool.com>
2016-05-23 10:57:11 -0700

    first commit

---
+++ swift_code_is_so_fast/3-longest_word.swift
@@ -0,0 +1,24 @@
+func longest_word(text: String) -> (String) {
+my_text += " "
+var word = ""
+var length = 0
+
+var max = 0
+var longestWord = ""
+
+for character in my_text.characters {
+  if character == " " {
+      if length > max {
+          max = length
+          longestWord = word
+      }
+      word = ""
+      length = 0
+  } else {
+      word += "\(character)"
+      length++
+  }
+}
+
+return (longestWord)
+}

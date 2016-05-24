3535f235605e7154e0881ef688b5fa7c076a1dfa
Kris Bredemeier <34@holbertonschool.com>
2016-05-24 10:49:05 -0700

    fist task

---
+++ manage_the_school_with_swift/person.swift
@@ -0,0 +1,19 @@
+var str = "Hello, playground"
+
+class Person{
+
+    var first_name: String
+    var last_name: String
+    var age: Int
+
+    init(first_name: String, last_name: String, age: Int) {
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    }
+
+    func fullName() -> String {
+        return first_name + " " + last_name
+    }
+
+}

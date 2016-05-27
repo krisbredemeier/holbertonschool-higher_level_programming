a673e54e74920fc2f32826a65adc22381779f8d6
Kris Bredemeier <34@holbertonschool.com>
2016-05-27 12:38:22 -0700

    first commit

---
+++ welcome_to_silicon_valley/SiliconValleyCompanies/SiliconValleyCompanies/TechCompaniesHelper.swift
@@ -0,0 +1,24 @@
+//
+//  TechCompaniesHelper.swift
+//  SiliconValleyCompanies
+//
+//  Created by Kristine Bredemeier on 5/27/16.
+//  Copyright © 2016 Kristine Bredemeier. All rights reserved.
+//
+
+import Foundation
+
+class TechCompaniesHelper {
+
+    static var companies:[String] = ["Holberton", "Linkedin", "Docker", "Google", "Yahoo", "Apple"]
+
+    init() {
+
+    }
+
+
+    static func getTechCompanies() -> String {
+        return self.companies
+    }
+
+}

a48fd5162aba71c53cce52a056bc0ced7d327aba
Kris Bredemeier <34@holbertonschool.com>
2016-06-03 10:37:28 -0700

    still needs tvshow

--- basic_sql_playground/5-main.sql
+++ basic_sql_playground/5-main.sql
@@ -5,6 +5,7 @@ where tvshow_id = 3;
 
 select count(*) From Person where age > 30;
 
-select * from person, eyescolor, tvshow;
+select * from person
+inner join (select tvshow_id, person_id as id from tvshowperson) using (id);
 
 select SUM(age) from Person;

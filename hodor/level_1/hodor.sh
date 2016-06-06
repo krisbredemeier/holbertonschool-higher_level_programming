30050b15cd3770f8dcf39fb130eeb50969ef665c
Kris Bredemeier <34@holbertonschool.com>
2016-06-06 15:47:43 -0700

    changes key to 1234

--- hodor/level_1/hodor.sh
+++ hodor/level_1/hodor.sh
@@ -1,4 +1,4 @@
 while :
 do
-curl 'http://173.246.108.142/level1.php' -H 'Cookie: HoldTheDoor=bacf6a86e6b9ae5ab3589f8dae362aac3de2d250' -H 'Connection: keep-alive' --data 'id=34&holdthedoor=Submit&key=bacf6a86e6b9ae5ab3589f8dae362aac3de2d250' --compressed
+curl 'http://173.246.108.142/level1.php' -H 'Cookie: HoldTheDoor=1234' -H 'Connection: keep-alive' --data 'id=34&holdthedoor=Submit&key=1234' --compressed
 done

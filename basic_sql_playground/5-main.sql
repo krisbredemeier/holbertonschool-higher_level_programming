5956647799a39d750961c6711a59ed8511a64033
Kris Bredemeier <34@holbertonschool.com>
2016-06-06 11:58:58 -0700

    cleans up 5

--- basic_sql_playground/5-main.sql
+++ basic_sql_playground/5-main.sql
@@ -5,7 +5,10 @@ where tvshow_id = 3;
 
 select count(*) From Person where age > 30;
 
-select * from person
-inner join (select tvshow_id, person_id as id from tvshowperson) using (id);
+select P.id, first_name, last_name, age, color, name from Person P
+	inner join EyesColor E on P.id=E.person_id
+	inner join TVShowPerson TVSP on P.id=TVSP.person_id
+	inner join TVShow TVS on TVS.id=TVSP.tvshow_id
+;
 
 select SUM(age) from Person;

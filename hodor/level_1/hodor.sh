1d318ebf417a493ae31d1305ae1b40a2c0905477
Kris Bredemeier <34@holbertonschool.com>
2016-06-06 15:50:06 -0700

    removes keep alive

--- hodor/level_1/hodor.sh
+++ hodor/level_1/hodor.sh
@@ -1,4 +1,4 @@
 while :
 do
-curl 'http://173.246.108.142/level1.php' -H 'Cookie: HoldTheDoor=1234' -H 'Connection: keep-alive' --data 'id=34&holdthedoor=Submit&key=1234' --compressed
+curl 'http://173.246.108.142/level1.php' -H 'Cookie: HoldTheDoor=1234' --data 'id=34&holdthedoor=Submit&key=1234' --compressed
 done

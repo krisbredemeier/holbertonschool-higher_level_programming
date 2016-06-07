c848c1037fd23dd134855b6009b21191900ed212
Kris Bredemeier <34@holbertonschool.com>
2016-06-06 22:42:35 -0700

    removes id

--- basic_sql_playground/3-main.sql
+++ basic_sql_playground/3-main.sql
@@ -1,6 +1,6 @@
-update person set age = 27 where id = 6;
-update person set age = 18 where id = 2;
-delete from eyescolor where person_id = 1;
-delete from person where id = 1;
+update Person set age=27 where first_name="Jon" AND last_name="Snow";
+update Person set age=18 where first_name="Walter Junior" AND last_name="White";
+delete from eyescolor where person_id=(select id from Person where first_name = "Waler" AND last_name = "White");
+delete from person where first_name = "Walter" AND last_name = "White";
 select * from person
   order by first_name;

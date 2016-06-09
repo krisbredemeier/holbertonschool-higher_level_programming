e4127c0ed2beef4e17ee207a9ba03c5037cafe0a
Kris Bredemeier <34@holbertonschool.com>
2016-06-09 13:52:19 -0700

    digital clock working

---
+++ community/clock/Views 101/Views 101/clock.swift
@@ -0,0 +1,18 @@
+//
+//  clock.swift
+//  Views 101
+//
+//  Created by Kristine Bredemeier on 6/9/16.
+//  Copyright © 2016 Kristine Bredemeier. All rights reserved.
+//
+
+
+import Foundation
+
+class Clock {
+
+    var currentTime: NSDate {
+        return NSDate()
+    }
+
+}

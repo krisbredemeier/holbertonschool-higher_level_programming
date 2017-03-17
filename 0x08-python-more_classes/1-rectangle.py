0f3f1d26a6b091d727da44fb39d1201d1966a931
Kris Bredemeier <krisbredemeier@gmail.com>
2017-03-17 21:55:28 +0000

    begins to build rectangle class

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,13 +2,27 @@
 ''' Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py) '''
 
 class Rectangle():
-
+    @property
     def width(self):
+        return self.__width
 
+    @width.setter
     def width(self, value):
-
+        if isinstance(value, int)
+            width < 0
+            ValueError (width must be >= 0)
+        TypeError(width must be an integer)
+    @property
     def height(self):
+        return self.__height
 
+    @height.setter
     def height(self, value):
+        if isinstance(value, int)
+            heigth < 0
+            ValueError(height must be >= 0)
+        TypeError(height must be an integer)
 
 def __init__(self, width=0, height=0):
+    self.width = width
+    self.height = height

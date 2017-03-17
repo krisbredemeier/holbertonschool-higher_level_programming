fb20e15df01ec9314118aa60410264d5c1b8cdd7
Kris Bredemeier <krisbredemeier@gmail.com>
2017-03-17 21:44:55 +0000

    requirements

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+''' Write a class Rectangle that defines a rectangle by: (based on 0-rectangle.py) '''
+
+class Rectangle():
+
+    def width(self):
+
+    def width(self, value):
+
+    def height(self):
+
+    def height(self, value):

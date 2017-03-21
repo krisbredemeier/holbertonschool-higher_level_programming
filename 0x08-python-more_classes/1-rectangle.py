bb359f53d650705d3a462ecb7d6c0e9670a11098
Kris Bredemeier <34@holbertonschool.com>
2017-03-21 14:39:44 -0700

    styling for pep8

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
+''' Write a class Rectangle that defines a rectangle '''
 
-''' Write a class Rectangle that defines a rectangle'''
 
-
-class Rectangle
+class Rectangle():
     @property
     def width(self):
         '''retrieve'''
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -13,10 +13,11 @@ class Rectangle():
         '''set'''
         if isinstance(value, int):
             if width < 0:
-                raise ValueError ("width must be >= 0")
+                raise ValueError("width must be >= 0")
             self.__width = value
         else:
-            raise TypeError ("width must be an integer")
+            raise TypeError("width must be an integer")
+
     @property
     def height(self):
         '''retrieve'''
@@ -27,10 +28,10 @@ class Rectangle():
         '''set'''
         if isinstance(value, int):
             if heigth < 0:
-                raise ValueError ("height must be >= 0")
+                raise ValueError("height must be >= 0")
             self.__height = value
         else:
-            raise TypeError ("height must be an integer")
+            raise TypeError("height must be an integer")
 
     def __init__(self, width=0, height=0):
         '''Instantiation'''
@@ -47,11 +48,10 @@ class Rectangle():
             return 0
         return (self.__width * 2) + (self.height * 2)
 
-
     def __str__(self):
         '''print() and str() should print the rectangle with the character #'''
         if self.__width == 0 or self.__height == 0:
-            raise ValueError ("width and height cannot equal 0")
+            raise ValueError("width and height cannot equal 0")
             return ''
         rectangle_representation = ('#' * self.__width + '\n') * self.__height
         return (rectangle_representation)

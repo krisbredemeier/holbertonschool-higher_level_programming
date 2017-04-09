ca0fae1ade52b365241dbd11f61379af97789fca
Kris Bredemeier <34@holbertonschool.com>
2017-04-09 10:50:48 -0700

    works on parsing commits

--- api_python_script.py
+++ api_python_script.py
@@ -3,20 +3,19 @@
 import urllib2
 import json
 
-# url= "https://github.com/holbertonschool/Betty"
-# response = urllib2.urlopen(url)
-# data = json.loads(response.read())
-# print data
-# commits.
-#     print "#{commit['date']}:#{commit['commit ID']['username']['commit message']}"
+
 
 def get_json(url):
     response = urllib2.urlopen(url)
     data = response.read().decode("utf-8")
     return json.loads(data)
+    commits = data[0]['commit']
+
+    for commit in commits:
+        print commit['commit']
 
 url= "https://api.github.com/repos/holbertonschool/Betty/commits"
 print(get_json(url))
-
+z
 
 # curl -i https://github.com/holbertonschool/Betty
